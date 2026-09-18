61241360c081b242928cf98ea74735aacff93624
ambientlyricism <minrayyang@gmail.com>
2026-09-18 13:59:37 -0500

    Update mouse input handling and return types

--- app/Madeira/GamepadBridge.swift
+++ app/Madeira/GamepadBridge.swift
@@ -70,17 +70,17 @@ final class GamepadBridge {
             
             mouseInput.mouseMovedHandler = {(_ mouse: GCMouseInput, _ deltaX: Float, _ deltaY: Float) -> Void in
                 winios_pointer(Int32(deltaX), Int32(deltaY), 0x0001, 0)
-                delta = CGPoint(x: deltaX, y: deltaY)                            
+                delta = CGPoint(x: CGFloat(deltaX), y: CGFloat(deltaY))                            
             }
            // mouseInput.scroll.valueChangedHandler = {
            //     (_ cursor: GCControllerDirectionPad, _ scrollX: Float, _ scrollY: Float) -> Void in
            //     winios_pointer(0, 0, 0x0800, UInt32(scrollY))
            // }
         }
-       func GCMouseInputX() {
+       func GCMouseInputX() -> Int32 {
           return Int32(delta.x)
        }
-       func GCMouseInputY() {
+       func GCMouseInputY() -> Int32 {
           return Int32(delta.y)
        }
     }
