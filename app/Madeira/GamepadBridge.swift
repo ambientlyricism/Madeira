88096f383ea6868f252331a07450f5989b6e2a17
ambientlyricism <minrayyang@gmail.com>
2026-09-20 01:36:18 -0500

    Call setNeedsUpdateOfPrefersPointerLocked in ViewDidLoad

--- app/Madeira/GamepadBridge.swift
+++ app/Madeira/GamepadBridge.swift
@@ -26,6 +26,7 @@ final class ViewController: UIViewController {
    }
    func ViewDidLoad() {
       GamepadBridge = Madeira.GamepadBridge()
+      setNeedsUpdateOfPrefersPointerLocked()
    }
 }
 final class GamepadBridge {
