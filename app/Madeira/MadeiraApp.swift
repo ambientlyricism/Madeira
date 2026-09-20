e158d5620759ec69947b74625b08ade41369800f
ambientlyricism <minrayyang@gmail.com>
2026-09-19 21:50:08 -0500

    Comment out unimplemented init(coder:) method
    Comment out the required init method in MadeiraApp.

--- app/Madeira/MadeiraApp.swift
+++ app/Madeira/MadeiraApp.swift
@@ -12,9 +12,9 @@ struct MadeiraApp: App {
         GamepadBridge.shared.start()
             // self.GamepadBridge = GamepadBridge()
         // }
-        required init?(coder: NSCoder) {
-		    fatalError("init(coder:) has not been implemented")
-	    }
+        // required init?(coder: NSCoder) {
+		//    fatalError("init(coder:) has not been implemented")
+	    // }
     }
 
     var body: some Scene {
