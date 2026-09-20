192e2c64730236131ffa255d060edab9aa326cb8
ambientlyricism <minrayyang@gmail.com>
2026-09-19 23:10:41 -0500

    Fix GamepadBridge variable type and clean up code
    Updated GamepadBridge variable type and removed unused initializers.

--- app/Madeira/MadeiraApp.swift
+++ app/Madeira/MadeiraApp.swift
@@ -5,17 +5,18 @@ import SceneKit
 @main
 struct MadeiraApp: App {
     final class ViewController: UIViewController {
-        var GamepadBridge: GameController?
+        var GamepadBridge: GamepadBridge?
         override var prefersPointerLocked: Bool {
             return true
         }
+		self.GamepadBridge = GamepadBridge()
 		// GamepadBridge()
-        init() {
-            self.GamepadBridge = GamepadBridge()
-        }
-        required init?(coder: NSCoder) {
-		   fatalError("init(coder:) has not been implemented")
-	    }
+        // init() {
+
+        // }
+        // required init?(coder: NSCoder) {
+		//   fatalError("init(coder:) has not been implemented")
+	    // }
     }
 
     var body: some Scene {
