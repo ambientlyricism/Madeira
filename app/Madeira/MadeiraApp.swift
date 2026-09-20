ebab35d388f78bc47626e28ae4c63f539ed1675f
ambientlyricism <minrayyang@gmail.com>
2026-09-19 23:53:26 -0500

    Fix GamepadBridge initialization in MadeiraApp

--- app/Madeira/MadeiraApp.swift
+++ app/Madeira/MadeiraApp.swift
@@ -1,18 +1,12 @@
 import SwiftUI
 import UIKit
-import SceneKit
 
 @main
 struct MadeiraApp: App {
-    final class ViewController: UIViewController {
-        var GamepadBridge: GamepadBridge?
-        override var prefersPointerLocked: Bool {
-            return true
-        }
 		// GamepadBridge = GamepadBridge()
 		// GamepadBridge()
-        override init() {
-			GampadBridge.shared.start()
+        init() {
+			GamepadBridge.shared.start()
         }
         // required init?(coder: NSCoder) {
 		//   fatalError("init(coder:) has not been implemented")
