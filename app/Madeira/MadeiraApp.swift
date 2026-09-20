776bbddcf5e25b73f91baa1a4aa530e4f88c4427
ambientlyricism <minrayyang@gmail.com>
2026-09-19 20:02:20 -0500

    Initialize GamepadBridge in MadeiraApp

--- app/Madeira/MadeiraApp.swift
+++ app/Madeira/MadeiraApp.swift
@@ -2,9 +2,9 @@ import SwiftUI
 
 @main
 struct MadeiraApp: App {
-    // init() {
-    //    GamepadBridge.shared.start()
-    // }
+    init() {
+        GamepadBridge.shared.start()
+    }
 
     var body: some Scene {
         WindowGroup {
