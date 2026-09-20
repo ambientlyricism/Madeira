4988f07ca5a0d8c55658844251a5294045e90716
ambientlyricism <minrayyang@gmail.com>
2026-09-20 00:55:10 -0500

    Initialize GamepadBridge in MadeiraApp

--- app/Madeira/MadeiraApp.swift
+++ app/Madeira/MadeiraApp.swift
@@ -2,6 +2,13 @@ import SwiftUI
 import UIKit
 
 @main
+final class ViewController: UIViewController {
+   var GamepadBridge: GamepadBridge? 
+   override var prefersPointerLocked: Bool {
+	   return true
+   }
+   GamepadBridge = GamepadBridge()
+}
 struct MadeiraApp: App {
 		// GamepadBridge = GamepadBridge()
 		// GamepadBridge()
@@ -9,7 +16,7 @@ struct MadeiraApp: App {
 		//   fatalError("init(coder:) has not been implemented")
 	    // }
     init() {
-		GamepadBridge()
+		GamepadBridge.shared.start()
 	}
     var body: some Scene {
         WindowGroup {
