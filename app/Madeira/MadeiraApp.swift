924b97302ecaf66ec157adbe6e0a499616b5a4a3
ambientlyricism <minrayyang@gmail.com>
2026-09-19 21:02:29 -0500

    Add ViewController to lock pointer in MadeiraApp

--- app/Madeira/MadeiraApp.swift
+++ app/Madeira/MadeiraApp.swift
@@ -1,7 +1,13 @@
 import SwiftUI
+import UIKit
 
 @main
 struct MadeiraApp: App {
+    final class ViewController: UIViewController {
+        override var prefersPointerLocked: Bool {
+            return true
+        }
+    }
     init() {
         GamepadBridge.shared.start()
     }
