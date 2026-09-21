2ebad87b8b3e568d088359b07460aa576e9e393f
ambientlyricism <minrayyang@gmail.com>
2026-09-21 17:09:00 -0500

    Change MetalViewController background color to clear

--- app/Madeira/ContentView.swift
+++ app/Madeira/ContentView.swift
@@ -35,7 +35,7 @@ struct MadeiraViewController: UIViewControllerRepresentable {
 
     func makeUIViewController(context: Context) -> MetalViewController  {
         let myViewController = MetalViewController()
-		// myViewController.view.backgroundColor = .blue
+		myViewController.view.backgroundColor = .clear
 		// let label = UILabel()
 		// label.text = "Controller Active"
 		// label.textColor = UIColor.red
