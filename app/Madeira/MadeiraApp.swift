4a1bb9d613219e9bdd95f38a6c496a64d7babd87
ambientlyricism <minrayyang@gmail.com>
2026-09-20 19:17:45 -0500

    Add MetalViewController and MadeiraViewController

--- app/Madeira/MadeiraApp.swift
+++ app/Madeira/MadeiraApp.swift
@@ -12,6 +12,24 @@ import UIKit
 //   	   GamepadBridge = GamepadBridge()
 //   }
 // }
+struct MadeiraViewController: UIViewControllerRepresentable {
+	func makeUIViewController(context: Context) -> MetalViewController {
+        return MetalViewController()
+    }
+}
+final class MetalViewController: UIViewController {
+	var MetalBackedView: MetalBackedView?
+	static let shared = MetalViewController()
+	var shouldLockPointer: Bool = true
+    override var prefersPointerLocked: Bool {
+		return self.shouldLockPointer
+	}
+	func lockPointer() {
+		self.shouldLockPointer = true
+		setNeedsUpdateOfPrefersPointerLocked()
+		MetalBackedView = MetalBackedView()
+	}
+}
 struct MadeiraApp: App {
 		// GamepadBridge = GamepadBridge()
 		// GamepadBridge()
