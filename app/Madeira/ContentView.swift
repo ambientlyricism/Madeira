d820b5b21cf77c4f723ee956181783d73be9d6f7
ambientlyricism <minrayyang@gmail.com>
2026-09-21 14:27:13 -0500

    Fix instantiation of MetalHostingController

--- app/Madeira/ContentView.swift
+++ app/Madeira/ContentView.swift
@@ -59,7 +59,7 @@ struct MadeiraViewController: UIViewControllerRepresentable {
 			return self.shouldLockPointer
 		}
 		func lockPointer() {
-			MetalHostingController = MetalHostingController(ContentView)
+			MetalHostingController = MetalHostingController(ContentView())
 			let MetalViewController = MetalHostingController.presentingViewController
 			self.shouldLockPointer = true
 			setNeedsUpdateOfPrefersPointerLocked()
