eea34bddbcec3f0b0c49baac654bee69f07378ad
ambientlyricism <minrayyang@gmail.com>
2026-09-21 05:09:01 -0500

    Comment out MadeiraViewController in WindowGroup

--- app/Madeira/MadeiraApp.swift
+++ app/Madeira/MadeiraApp.swift
@@ -34,7 +34,7 @@ struct MadeiraApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
-			MadeiraViewController()
+			// MadeiraViewController()
         }
     }
 }
