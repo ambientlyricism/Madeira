70ab2ce8fd81ccb22b9408de30ae2210f0ad6e2e
ambientlyricism <minrayyang@gmail.com>
2026-09-21 15:07:01 -0500

    Reposition MadeiraViewController in ContentView
    Moved MadeiraViewController to the bottom of the ZStack.

--- app/Madeira/ContentView.swift
+++ app/Madeira/ContentView.swift
@@ -1082,7 +1082,6 @@ struct ContentView: View {
             let gameW = min(geo.size.width, geo.size.height * 4.0 / 3.0)
             let barW = max((geo.size.width - gameW) / 2.0, 44)
             ZStack {
-				MadeiraViewController()
                 Color.black
                 MadeiraMetalView()
                     .onAppear { TouchControlsHost.attach() }
@@ -1102,6 +1101,7 @@ struct ContentView: View {
                     }
                     .frame(width: barW)
                 }
+				MadeiraViewController()
             }
         }
         .ignoresSafeArea()
