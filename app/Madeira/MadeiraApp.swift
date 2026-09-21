70a8fcf5abfefc9900db73a5c2b732f5a5c323fa
ambientlyricism <minrayyang@gmail.com>
2026-09-20 21:00:48 -0500

    Override viewDidLoad in MadeiraApp
    Add viewDidLoad method to override for additional setup.

--- app/Madeira/MadeiraApp.swift
+++ app/Madeira/MadeiraApp.swift
@@ -33,6 +33,9 @@ final class MetalViewController: UIViewController {
 		setNeedsUpdateOfPrefersPointerLocked()
 		MetalBackedView = MetalBackedView()
 	}
+	override func viewDidLoad() {
+       super.viewDidLoad()
+	}
 }
 struct MadeiraApp: App {
 		// GamepadBridge = GamepadBridge()
