c47472db2e4c2515f627a2f760bdd20b36e9fb78
ambientlyricism <minrayyang@gmail.com>
2026-09-22 01:13:53 -0500

    Change WindowGroup to use fullScreenCover for MadeiraViewController

--- app/Madeira/MadeiraApp.swift
+++ app/Madeira/MadeiraApp.swift
@@ -33,7 +33,7 @@ struct MadeiraApp: App {
 	}
     var body: some Scene {
         WindowGroup {
-			MadeiraViewController()
+			.fullScreenCover(MadeiraViewController())
             // ContentView()
         }
     }
