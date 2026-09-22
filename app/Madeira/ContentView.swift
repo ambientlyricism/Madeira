5a46fae2eee9a84fa09e4054997f1cd06d63d48c
ambientlyricism <minrayyang@gmail.com>
2026-09-22 06:04:28 -0500

    Register mouse with GamepadBridge in ContentView

--- app/Madeira/ContentView.swift
+++ app/Madeira/ContentView.swift
@@ -470,6 +470,7 @@ final class MetalBackedView: UIView {
             if ix != 0 || iy != 0 { winios_pointer(ix, iy, F_MOVE, 0) }
             return
         }
+		GamepadBridge.shared.registerMouse()
 		guard !GamepadBridge.shared.MouseActive() else {return}
         let sens = CGFloat(InputSettings.shared.sensAbs)   // desktop px per view pt
         let maxX = CGFloat(envInt("MADEIRA_SCREEN_W", 1024) - 1)
