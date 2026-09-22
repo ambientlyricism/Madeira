54a1337183a2f15c203339e30b899c25c7a93c52
ambientlyricism <minrayyang@gmail.com>
2026-09-22 07:47:03 -0500

    Modify hostingController view constraints
    Updated constraints for hostingController view.

--- app/Madeira/ContentView.swift
+++ app/Madeira/ContentView.swift
@@ -85,11 +85,11 @@ final class MetalViewController: UIViewController {
         addChild(hostingController)
         hostingController.view.frame = view.bounds
         view.addSubview(hostingController.view)
-		// hostingController.view.translatesAutoresizingMaskIntoConstraints = false
-		// hostingController.view.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
-		// hostingController.view.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
-		// hostingController.view.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
-		// hostingController.view.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+		hostingController.view.translatesAutoresizingMaskIntoConstraints = false
+		hostingController.view.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = false
+		hostingController.view.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = false
+		hostingController.view.topAnchor.constraint(equalTo: view.topAnchor).isActive = false
+		hostingController.view.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = false
         hostingController.didMove(toParent: self)
 		GamepadBridge = Madeira.GamepadBridge()
 		
