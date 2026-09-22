2998d5b00d488a404a6446cb59f4191a2d9fd4b1
ambientlyricism <minrayyang@gmail.com>
2026-09-22 02:59:30 -0500

    Fix safeAreaRegions assignment in ContentView

--- app/Madeira/ContentView.swift
+++ app/Madeira/ContentView.swift
@@ -77,7 +77,7 @@ final class MetalViewController: UIViewController {
 		let ContentView = ContentView()
         let hostingController = MetalHostingController(rootView: ContentView)
 		if #available(iOS 16.4, *) {
-   			hosting.safeAreaRegions = []
+   			hostingController.safeAreaRegions = []
 		}
         
         addChild(hostingController)
