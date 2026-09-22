0ad95a5d9ea9e0ef65cb2cb0234a026c683da5f2
ambientlyricism <minrayyang@gmail.com>
2026-09-22 06:35:50 -0500

    Comment out mouse registration in ContentView.swift
    Comment out the mouse registration line in ContentView.swift.

--- app/Madeira/ContentView.swift
+++ app/Madeira/ContentView.swift
@@ -470,7 +470,7 @@ final class MetalBackedView: UIView {
             if ix != 0 || iy != 0 { winios_pointer(ix, iy, F_MOVE, 0) }
             return
         }
-		GamepadBridge.shared.registerMouse(GCMouse)
+		// GamepadBridge.shared.registerMouse(GCMouse)
 		guard !GamepadBridge.shared.MouseActive() else {return}
         let sens = CGFloat(InputSettings.shared.sensAbs)   // desktop px per view pt
         let maxX = CGFloat(envInt("MADEIRA_SCREEN_W", 1024) - 1)
