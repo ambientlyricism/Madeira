9cfd4be29f7956ff61d75a0d90095bb70bf0c95e
ambientlyricism <minrayyang@gmail.com>
2026-09-23 05:10:12 -0500

    Change GamepadBridge to an optional variable

--- app/Madeira/MadeiraApp.swift
+++ app/Madeira/MadeiraApp.swift
@@ -22,7 +22,7 @@ import SwiftUI
 // }
     
 struct MadeiraApp: App {
-		GamepadBridge = GamepadBridge()
+		var GamepadBridge: GamepadBridge?
 		// GamepadBridge()
         // required init?(coder: NSCoder) {
 		//   fatalError("init(coder:) has not been implemented")
