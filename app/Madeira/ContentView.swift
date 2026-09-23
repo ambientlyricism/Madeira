593856b24dcd4d0ca3318c411c43ddd30799e01e
ambientlyricism <minrayyang@gmail.com>
2026-09-23 18:03:48 -0500

    Comment out currentLock variable initialization

--- app/Madeira/ContentView.swift
+++ app/Madeira/ContentView.swift
@@ -38,7 +38,7 @@ struct PointerView: View {
 	}
 }
 */
-public var currentLock: String = "Initiating"
+// public var currentLock: String = "Initiating"
 struct MadeiraUIViewController: UIViewControllerRepresentable, UIWindow {
         
     typealias UIViewControllerType = MetalUIViewController
