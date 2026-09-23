de2a7d443b801bb75c9c9774b0dc18da30510a8e
ambientlyricism <minrayyang@gmail.com>
2026-09-23 15:43:18 -0500

    Remove AppDelegate and SceneDelegate classes
    Removed AppDelegate and SceneDelegate classes from ContentView.swift.

--- app/Madeira/ContentView.swift
+++ app/Madeira/ContentView.swift
@@ -39,23 +39,6 @@ struct PointerView: View {
 }
 */
 
-class AppDelegate: NSObject, UIApplicationDelegate {
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        let configuration = UISceneConfiguration(name: nil, sessionRole: connectingSceneSession.role)
-        configuration.delegateClass = SceneDelegate.self
-        return configuration
-    }
-}
-
-class SceneDelegate: NSObject, UIWindowSceneDelegate, ObservableObject { // Make SceneDelegate conform ObservableObject
-    var window: UIWindow?
-
-    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        self.window = (scene as? UIWindowScene)?.keyWindow
-    }
-}
-
-
 struct MadeiraUIViewController: UIViewControllerRepresentable {
         
     typealias UIViewControllerType = MetalUIViewController
