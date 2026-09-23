103b36661f08ad73a7b8b24f5d1f665289917bc1
ambientlyricism <minrayyang@gmail.com>
2026-09-23 01:45:19 -0500

    Enable Auto Layout for hostingController view
    Updated layout constraints for hostingController view.

--- app/Madeira/ContentView.swift
+++ app/Madeira/ContentView.swift
@@ -99,13 +99,13 @@ final class MetalViewController: UIViewController {
 		self.navigationController?.isNavigationBarHidden = true
 		
         addChild(hostingController)
-        // hostingController.view.frame = view.bounds
         view.addSubview(hostingController.view)
-		// hostingController.view.translatesAutoresizingMaskIntoConstraints = false
-		// hostingController.view.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
-		// hostingController.view.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
-		// hostingController.view.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
-		// hostingController.view.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+		hostingController.view.translatesAutoresizingMaskIntoConstraints = false
+		hostingController.view.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+		hostingController.view.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+		hostingController.view.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+		hostingController.view.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+		hostingController.view.frame = view.bounds
         hostingController.didMove(toParent: self)
 		// GamepadBridge = Madeira.GamepadBridge()
 		lockPointer()
