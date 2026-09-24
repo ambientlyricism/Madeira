d8b152a1a05df86ea1dc7f1f3a99a53841569657
ambientlyricism <minrayyang@gmail.com>
2026-09-23 20:42:48 -0500

    Comment out UILabel setup in ContentView
    Comment out label creation and related properties.

--- app/Madeira/ContentView.swift
+++ app/Madeira/ContentView.swift
@@ -70,10 +70,10 @@ struct MadeiraUIViewController: UIViewControllerRepresentable {
         // myViewController.delegate = context.coordinator
 		// myViewController.view.isOpaque = false
 		// myViewController.view.backgroundColor = .clear
-		let label = UILabel()
+		// let label = UILabel()
 		// let PointerLockNotice = PointerLockNotice.shared.status()
-		label.text = currentLock
-		label.textColor = UIColor.red
+		// label.text = currentLock
+		// label.textColor = UIColor.red
 		// myViewController.view.addSubview(label)
 		// label.frame = myViewController.view.frame
         // myViewController.delegate = context.coordinator
